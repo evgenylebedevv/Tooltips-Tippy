a03fe0eeda5d14e634873a8d1c168a3a2161d44e
Evgeny Lebedev <EvgenyLebedevv@gmail.com>
2023-03-17 22:29:21 +0300

    First

---
+++ app.js
@@ -0,0 +1,11 @@
+tippy('#myButton', {
+    theme: 'light',
+    animation: 'scale',
+    content: 'My BIG  COOOOMMENTT!!',
+});
+
+tippy('#move1', {
+    theme: 'light',
+    animation: 'scale',
+    content: 'Подсказка!'
+});
